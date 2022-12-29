0eec17e544c18bb6a166f8c45378c7fc4477c39a
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-12-29 13:59:47 +0100

    Formatting

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -10,26 +10,26 @@
 
 local status_ok, copilot = pcall(require, "copilot")
 if not status_ok then
-  return
+	return
 end
 
-copilot.setup {
-  cmp = {
-    enabled = true,
-    method = "getCompletionsCycling",
-  },
-  panel = { -- no config options yet
-    enabled = true,
-  },
-  ft_disable = { "markdown" },
-  -- plugin_manager_path = vim.fn.stdpath "data" .. "/site/pack/packer",
-  server_opts_overrides = {
-    -- trace = "verbose",
-    settings = {
-      advanced = {
-        listCount = 3, -- #completions for panel
-        inlineSuggestCount = 3, -- #completions for getCompletions
-      },
-    },
-  },
-}
+copilot.setup({
+	cmp = {
+		enabled = true,
+		method = "getCompletionsCycling",
+	},
+	panel = { -- no config options yet
+		enabled = true,
+	},
+	ft_disable = { "markdown" },
+	-- plugin_manager_path = vim.fn.stdpath "data" .. "/site/pack/packer",
+	server_opts_overrides = {
+		-- trace = "verbose",
+		settings = {
+			advanced = {
+				listCount = 3, -- #completions for panel
+				inlineSuggestCount = 3, -- #completions for getCompletions
+			},
+		},
+	},
+})
